3f4c6d38445565ac24239625ddfb7010a3cfa362
Sasank Jampana <sasankjampana@Sasanks-MacBook-Air.local>
2025-03-11 20:52:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let name = "John";
+let age = 25;
+let colors = ["Red", "Blue", "Green"];
+let person = { name: "John", age: 25, favoriteColor: "Blue" };
+
+console.log(name, age);
+console.log(colors);
+console.log(person);
